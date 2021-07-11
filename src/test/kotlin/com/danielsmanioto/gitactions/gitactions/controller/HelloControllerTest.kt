6cfbdfcfc1ae7d33b633ab003ad94896f732627b
Daniel Smanioto <daniel.smanioto@gmail.com>
2021-07-11 15:14:18 -0300

    add coverrage

---
+++ src/test/kotlin/com/danielsmanioto/gitactions/gitactions/controller/HelloControllerTest.kt
@@ -0,0 +1,31 @@
+package com.danielsmanioto.gitactions.gitactions.controller
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class HelloControllerTest {
+
+    @Autowired
+    private val mvc: MockMvc? = null
+
+    @Test
+    @DisplayName("Validate Hello - GET - Sucess")
+    fun validateHelloGetSuccess() {
+        mvc!!.perform(MockMvcRequestBuilders.get("/hello/daniel")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk)
+                .andExpect(content().string("daniel"))
+    }
+
+}
